e2253fc95bf7211f4bb865f4df00679999213e42
Kahlil Enmond <slope22@hotmail.com>
2020-06-18 14:29:24 -0400

    added katas 1-10 appends

---
+++ main.js
@@ -0,0 +1,43 @@
+const mainElement = document.querySelector(".Kata-List")
+
+// Kata 1
+const kata1Heading = document.createElement('h3')
+mainElement.append(kata1Heading)
+kata1Heading.append('Kata 1')
+
+// Kata 2
+const kata2Heading = document.createElement('h3')
+mainElement.append(kata2Heading)
+kata2Heading.append('Kata 2')
+// Kata 3
+const kata3Heading = document.createElement('h3')
+mainElement.append(kata3Heading)
+kata3Heading.append('Kata 3')
+// Kata 4
+const kata4Heading = document.createElement('h3')
+mainElement.append(kata4Heading)
+kata4Heading.append('Kata 4')
+// Kata 5
+const kata5Heading = document.createElement('h3')
+mainElement.append(kata5Heading)
+kata5Heading.append('Kata 5')
+// Kata 6
+const kata6Heading = document.createElement('h3')
+mainElement.append(kata6Heading)
+kata6Heading.append('Kata 6')
+// Kata 7
+const kata7Heading = document.createElement('h3')
+mainElement.append(kata7Heading)
+kata7Heading.append('Kata 7')
+// Kata 8
+const kata8Heading = document.createElement('h3')
+mainElement.append(kata8Heading)
+kata8Heading.append('Kata 8')
+// Kata 9
+const kata9Heading = document.createElement('h3')
+mainElement.append(kata9Heading)
+kata9Heading.append('Kata 9')
+// Kata 10
+const kata10Heading = document.createElement('h3')
+mainElement.append(kata10Heading)
+kata10Heading.append('Kata 10')
